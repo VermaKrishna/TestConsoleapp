24127ef5014ca8aecd29ff2e1b4ef79a0bb55dae
krverm <krverm@microsoft.com>
2023-07-07 10:13:12 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace TestConsoleapp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
